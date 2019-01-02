c3ad3e3e058c57307265036937dce6d5f94afe53
FreelancerAnkit <palliankit123@gmail.com>
2019-01-02 18:38:52 +0530

    Updated with Firebase Push Notification

--- app/src/main/java/databinding/rapidera/firebasedemo/firebase/FirebasePushMessages.java
+++ app/src/main/java/databinding/rapidera/firebasedemo/firebase/FirebasePushMessages.java
@@ -1,9 +1,25 @@
 package databinding.rapidera.firebasedemo.firebase;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import databinding.rapidera.firebasedemo.MainActivity;
+import databinding.rapidera.firebasedemo.R;
 
 import static android.content.ContentValues.TAG;
 
@@ -13,10 +29,57 @@ import static android.content.ContentValues.TAG;
 
 public class FirebasePushMessages  extends FirebaseMessagingService {
 
+    Bitmap bitmap;
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        bitmap = getBitmapfromUrl("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTStIcONDWk0GjyJewYfJmRUCqqFEn9_752g2TL8nW-bZEkCFqJg");
+
+        sendNotification("New NotificationTitle", bitmap);
     }
 
 
+    private void sendNotification(String messageBody, Bitmap image) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(messageBody)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(image))/*Notification with Image*/
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    /*
+    *To get a Bitmap image from the URL received
+    * */
+    public Bitmap getBitmapfromUrl(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(input);
+            return bitmap;
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+
+        }
+    }
 }
